25a618e681598f60a54cddcf33a0332e8897742a
Mike Acton <macton@gmail.com>
2012-05-12 09:44:27 +0000

    Initial working monochrome output-only for ecma-48/ansi escape code port.

---
+++ ecma-48/pdcclip.c
@@ -0,0 +1,20 @@
+int PDC_clearclipboard(void)
+{
+  return (1);
+}
+
+int PDC_freeclipboard(char *contents)
+{
+  return (1);
+}
+
+int PDC_getclipboard(char **contents, long *length)
+{
+  return (1);
+}
+
+int PDC_setclipboard(const char *contents, long length)
+{
+  return (1);
+}
+
